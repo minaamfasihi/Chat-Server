1060cd3f7b2f164044c65f798478f66247ec4fb9
minaamfasihi <minaam.fasihi@gmail.com>
2017-11-09 15:42:30 +0500

    Wrote the Clients List API

--- TestUdpServer/TestUdpServer/ClientsList.cs
+++ TestUdpServer/TestUdpServer/ClientsList.cs
@@ -8,10 +8,80 @@ namespace TestUdpServer
 {
     class ClientsList
     {
-        private List<string> senderClientsListProducer = null;
-        private List<string> senderClientsListConsumer = null;
-        private List<string> senderClientsList1 = new List<string>();
-        //private List<string>
-        private static object senderClientsListLock = new object();
+        private List<string> _senderClientsProducerList = null;
+        private List<string> _senderClientsConsumerList = null;
+        private List<string> _senderClientsList1 = new List<string>();
+        private List<string> _senderClientsList2 = new List<string>();
+        private static object senderClientsProducerLock = new object();
+        private static object senderClientsConsumerLock = new object();
+
+        public ClientsList()
+        {
+            _senderClientsProducerList = _senderClientsList1;
+            _senderClientsConsumerList = _senderClientsList2;
+        }
+
+        public void InsertInSenderClientsProducerList(string clientName)
+        {
+            lock (senderClientsProducerLock)
+            {
+                if (!_senderClientsProducerList.Contains(clientName))
+                {
+                    _senderClientsProducerList.Add(clientName);
+                }
+            }
+        }
+
+        public List<string> SenderConsumerList
+        {
+            get { return _senderClientsConsumerList; }
+        }
+
+        public void RemoveFromConsumerList(string clientName)
+        {
+            lock (senderClientsConsumerLock)
+            {
+                if (_senderClientsConsumerList.Contains(clientName))
+                {
+                    _senderClientsConsumerList.Remove(clientName);
+                }
+            }
+        }
+
+        public void SwapProducerList()
+        {
+            if (_senderClientsProducerList == _senderClientsList1)
+            {
+                _senderClientsProducerList = _senderClientsList2;
+            }
+            else
+            {
+                _senderClientsProducerList = _senderClientsList1;
+            }
+        }
+
+        public void SwapConsumerList()
+        {
+            if (_senderClientsConsumerList == _senderClientsList1)
+            {
+                _senderClientsConsumerList = _senderClientsList2;
+            }
+            else
+            {
+                _senderClientsConsumerList = _senderClientsList1;
+            }
+        }
+
+        public void SwapProducerConsumerList()
+        {
+            lock (senderClientsProducerLock)
+            {
+                lock (senderClientsConsumerLock)
+                {
+                    SwapProducerList();
+                    SwapConsumerList();
+                }
+            }
+        }
     }
 }
